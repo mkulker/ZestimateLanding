6800410aa15ef4eed974b7b2bcfdec4234c55ee0
mkulker <mkulker@umass.edu>
2024-07-03 13:46:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const url = 'https://zillow-com1.p.rapidapi.com/zestimate?address=9%20grandview%20road%20bow%20nh';
+const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '7aec46f57dmshe6448bbc1b6e213p17cf3fjsncd23c858fdc6',
+        'x-rapidapi-host': 'zillow-com1.p.rapidapi.com'
+    }
+};
+
+async function fetchZestimate() {
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const result = await response.json();
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+// fetchZestimate();
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <h2>Welcome to My Mobile-Friendly React Website</h2>
+        <p>This website is designed with mobile-first principles.</p>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>My Mobile React Website</h1>
+    </header>
+  );
+}
+
+export default Header;
